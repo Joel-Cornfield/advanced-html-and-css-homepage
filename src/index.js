ea76212041f33b356c82672e4ac8053b9e19d923
Joel-Cornfield <157437180+Joel-Cornfield@users.noreply.github.com>
2025-01-08 10:48:37 +0800

    initial commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles.css';
-
-function component() {
-  const element = document.createElement('div');
-  element.textContent = 'Hello, Project Template!';
-  return element;
-}
-
-document.getElementById('app').appendChild(component());
